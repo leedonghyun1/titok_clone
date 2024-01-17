105a26bdc32c9ff7ebb2c5f5854118cf15a6b4a5
leedonghyun1 <ehdgus4119@naver.com>
2024-01-17 16:40:26 +0900

    2024.01.17_Authentication Screen

---
+++ lib/features/authentication/birthday_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:titok_clone/constants/gaps.dart';
+import 'package:titok_clone/constants/sizes.dart';
+import 'package:titok_clone/features/authentication/widgets/form_button.dart';
+import 'package:titok_clone/features/onboarding/interest_screen.dart';
+
+class BirthdayScreen extends StatefulWidget {
+  const BirthdayScreen({super.key});
+
+  @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+  // TextField 를 controll 하기 위해서 -> 그리고 EventListener를 추가해서 TextField의 변화를 감지해야함.
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+
+  DateTime initialDate = DateTime.now();
+
+  @override
+  // controller 추가 후 listener를 위한 초기화
+  // initState 후에 controller 추가, dispose는 controller 를 dispose를 하고 super.dispose 진행.
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    // 다 정리 후에 super.dispose() 진행.
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(
+      text: textDate,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "What's your birthday?",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              'Your birthday wont shown publicly',
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                    width: Sizes.size2,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            GestureDetector(
+              onTap: _onNextTap,
+              child: const FormButton(disabled: false),
+            ),
+            // isEmpty라면 true를 반환
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 300,
+        child: CupertinoDatePicker(
+          maximumDate: initialDate,
+          initialDateTime: initialDate,
+          onDateTimeChanged: _setTextFieldDate,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/authentication/password_screen.dart
+++ lib/features/authentication/password_screen.dart
@@ -1,10 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:titok_clone/constants/gaps.dart';
+import 'package:titok_clone/constants/sizes.dart';
+import 'package:titok_clone/features/authentication/birthday_screen.dart';
+import 'package:titok_clone/features/authentication/widgets/form_button.dart';
 
-class PasswordScreen extends StatelessWidget {
+class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
 
   @override
+  State<PasswordScreen> createState() => _PasswordScreenState();
+  // TextField 를 controll 하기 위해서 -> 그리고 EventListener를 추가해서 TextField의 변화를 감지해야함.
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+
+  String _password = "";
+  bool _obscureText = true;
+
+  @override
+  // controller 추가 후 listener를 위한 초기화
+  // initState 후에 controller 추가, dispose는 controller 를 dispose를 하고 super.dispose 진행.
+  void initState() {
+    super.initState();
+    _passwordController.addListener(
+      () {
+        setState(
+          () {
+            _password = _passwordController.text;
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    // 다 정리 후에 super.dispose() 진행.
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  bool _isPasswordValid() {
+    return _password.isNotEmpty && _password.length > 8;
+  }
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (!_isPasswordValid()) return;
+    // password 비었거나 유효하지 않으면 return 반환
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const BirthdayScreen(),
+      ),
+    );
+  }
+
+  void _onClearTap() {
+    _passwordController.clear();
+  }
+
+  void _toggleObscureText() {
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sign Up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                'Password',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                controller: _passwordController,
+                onEditingComplete: _onSubmit,
+                obscureText: _obscureText,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        onTap: _onClearTap,
+                        child: Icon(
+                          Icons.cancel_rounded,
+                          color: Colors.red.shade200,
+                        ),
+                      ),
+                      Gaps.h10,
+                      GestureDetector(
+                        onTap: _toggleObscureText,
+                        child: Icon(
+                          _obscureText ? Icons.remove_red_eye : Icons.clear_all,
+                          color: Colors.red.shade200,
+                          size: Sizes.size28,
+                        ),
+                      ),
+                    ],
+                  ),
+                  hintText: 'Make it strong',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: Sizes.size2,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v16,
+              const Text(
+                'Your password must have : ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v10,
+              Row(
+                children: [
+                  Icon(
+                    Icons.check_circle,
+                    color: _isPasswordValid()
+                        ? Colors.green
+                        : Colors.grey.shade300,
+                  ),
+                  Gaps.h5,
+                  const Text(
+                    '8 to 20 characters',
+                  ),
+                ],
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmit,
+                child: FormButton(
+                  disabled: !_isPasswordValid(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
