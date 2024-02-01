0643b54a1906538ae4ced49440c7c949ac60e424
leedonghyun1 <ehdgus4119@naver.com>
2024-02-01 23:52:26 +0900

    Add user_profile_screen

---
+++ lib/features/users/user_profile_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              "assets/images/placeholder.jpg",
+              fit: BoxFit.fill,
+            ),
+            title: const Text(
+              "Hello",
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
